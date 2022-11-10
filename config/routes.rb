5f330c01b7cce938abb5d4135f73da1c77a3e1bb
OybekKayumov <oybek.kayumov@gmail.com>
2022-11-10 18:12:17 +0500

    Add LineItem

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :line_items
+  resources :carts
   # get 'store/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
