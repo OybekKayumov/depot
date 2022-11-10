722a40edfb138287bd2512087dd1bbf50e9a57d0
OybekKayumov <oybek.kayumov@gmail.com>
2022-11-10 17:11:57 +0500

    Add catalog display

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  resources :products
+  # get 'store/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  root 'store#index', as: 'store_index'
+  resources :products
 end
