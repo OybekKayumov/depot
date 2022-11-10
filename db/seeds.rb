92d5561bc508a6d1e5207835dbe7ede0906d92b5
OybekKayumov <oybek.kayumov@gmail.com>
2022-11-10 15:27:21 +0500

    Update seeds and images

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,4 +24,37 @@ skills for communicating, information, and fun.
 </p>},
 image_url: 'dpchat.jpg', 
 price: 20.00)
+
+Product.create!(title: 'Docker for Rails Developers',
+  description:
+    %{<p>
+      <em>Build, Ship, and Run Your Applications Everywhere</em>
+      Docker does for DevOps what Rails did for web development—it gives you 
+      a new set of superpowers. Gone are “works on my machine” woes and lengthy 
+      setup tasks, replaced instead by a simple, consistent, Docker-based 
+      development environment that will have your team up and running in seconds. 
+      Gain hands-on, real-world experience with a tool that’s rapidly becoming 
+      fundamental to software development. Go from zero all the way to production 
+      as Docker transforms the massive leap of deploying your app in the cloud 
+      into a baby step.
+      </p>},
+  image_url: 'ridocker.jpg',
+  price: 38.00)
+# . . .
+
+Product.create!(title: 'Programming Crystal',
+  description:
+    %{<p>
+      <em>Create High-Performance, Safe, Concurrent Apps</em>
+      Crystal is for Ruby programmers who want more performance or for 
+      developers who enjoy working in a high-level scripting environment. Crystal 
+      combines native execution speed and concurrency with Ruby-like syntax, so 
+      you will feel right at home. This book, the first available on Crystal, 
+      shows you how to write applications that have the beauty and elegance of a 
+      modern language, combined with the power of types and modern concurrency 
+      tooling. Now you can write beautiful code that runs faster, scales better, 
+      and is a breeze to deploy.
+      </p>},
+  image_url: 'crystal.jpg',
+  price: 40.00)
 #...
